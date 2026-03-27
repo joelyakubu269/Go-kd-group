f521bb48428392db9732fe00e4f5d1b2c5b9fbb7
joelyakubu269 <joelyakubu269@gmail.com>
2026-03-27 15:27:37 +0100

    basic looping in go

---
+++ warmup.go
@@ -0,0 +1,23 @@
+// package main
+
+// import(
+// 	"fmt"
+// )
+// func main(){
+// 	for i:=1; i<=10;i++ {
+// 		fmt.Print(i ," ")
+// 	}
+// }
+package main
+
+import(
+	"fmt"
+)
+func main() {
+	for i:=1; i<=10;i++ {
+		if i % 2 == 0 { 
+					fmt.Print(i ," ")
+
+		}
+	}	
+}
